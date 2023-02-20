6c06e3fb57ad0eb101cf2407cc6a52f68846c017
abhayfaldu <abhayfaldu1922@gmail.com>
2023-02-20 17:38:33 +0530

    completed user login route

--- routes/User.route.js
+++ routes/User.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const { UserModel } = require("../models/User.model");
 const userRoute = express.Router();
+const bcrypt = require("bcrypt");
 
 // name ==> String
 // email ==> String
@@ -28,16 +30,18 @@ userRoute.get("/", async (req, res) => {
 
 userRoute.post("/register", async (req, res) => {
 	const { name, email, password, gender, age, city } = req.body;
-	const checkIfRegistered = await UserModel.findOne({ email: email });
-	if (!checkIfRegistered.length) {
-		try {
+	try {
+		const users = await UserModel.find({
+			email: email,
+		});
+		if (users.length <= 0) {
 			bcrypt.hash(password, 3, async (err, hash) => {
 				if (err) res.send({ msg: "some error", error: err.message });
 				else {
 					const user = new UserModel({
 						name,
 						email,
-						password,
+						password: hash,
 						gender,
 						age,
 						city,
@@ -46,18 +50,34 @@ userRoute.post("/register", async (req, res) => {
 					res.send({ msg: "success registered" });
 				}
 			});
-		} catch (error) {
-			res.send({ msg: "some error", error: error.message });
+		} else {
+			res.send("User already exist, please login");
 		}
+	} catch (error) {
+		res.send({ msg: "some error", error: error.message });
 	}
 });
 
 userRoute.post("/login", async (req, res) => {
 	const { email, pass } = req.body;
 	try {
-		await UserModel.findOne();
-	} catch (err) {
-		console.log({ msg: "some error", err: err.message });
+		const users = await UserModel.find({
+			email: email,
+		});
+		if (users.length > 0) {
+			bcrypt.compare(pass, users[0].password, (err, result) => {
+				if (result) {
+					const token = jwt.sign({ userId: users[0]._id }, "linkedin");
+					res.send({ msg: "logged in", token: token });
+				} else {
+					res.send({ msg: "some error" });
+				}
+			});
+		} else {
+			res.send("User not found, sign up");
+		}
+	} catch (error) {
+		res.send({ msg: "some error", error: error.message });
 	}
 });
 
